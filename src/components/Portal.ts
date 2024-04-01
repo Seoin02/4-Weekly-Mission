6cc2f9d7432a285d87398987f96dc72c34826c17
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-01 20:24:08 +0900

    feat: folder 페이지 렌더링

--- src/components/Portal.ts
+++ src/components/Portal.ts
@@ -1,17 +1,7 @@
-import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const ModalPortal = ({ children }: { children: React.ReactNode[] }) => {
-  const modal = document.getElementById('modal');
-  const element = document.getElementById('div');
-
-  useEffect(() => {
-    modal?.appendChild(element);
-    return () => {
-      modal?.removeChild(element);
-    };
-  }, [element, modal]);
-
+  const element = document.getElementById('modal');
   return !element ? null : ReactDOM.createPortal(children, element);
 };
 
