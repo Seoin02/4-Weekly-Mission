a861205925ec1c4064336efcfe13fd58fe498724
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-13 23:14:13 +0900

    feat: input 공용 컴포넌트 구현

---
+++ src/components/seoin/Input.tsx
@@ -0,0 +1,41 @@
+import styles from "./FormSeoin.module.css";
+
+interface InputProps {
+  id: string;
+  type: string;
+  label?: string;
+  value?: string | number;
+  style?: string;
+  isError: boolean;
+  errorText?: string;
+  labelStyle?: string;
+  placeholder: string;
+}
+
+export default function InputSeoin({
+  id,
+  type,
+  label,
+  value,
+  style,
+  isError,
+  errorText,
+  labelStyle,
+  placeholder,
+}: InputProps) {
+  return (
+    <div>
+      <label htmlFor={id} className={labelStyle}>
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        className={`${style} ${isError && styles["Error"]}`}
+        placeholder={placeholder}
+      />
+      {isError && <p className={styles.ErrorText}>{errorText}</p>}
+    </div>
+  );
+}
